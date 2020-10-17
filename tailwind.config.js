d0432332a08eff1f44698f79bd21e87147bedf75
Christoph Gockel <git@christophgockel.de>
2020-10-17 20:58:51 +0100

    Adds basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#345C67",
+        yellow: "#FFF3B0",
+        orange: "#E09F3E",
+        red: "#9E2B2B",
+        dark: "#0C1518",
+      },
+    },
   },
   variants: {},
   plugins: [],
